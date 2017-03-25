1891a59636240d93f8c479e2f43deea21b403187
Rui Fonseca <ruimcf95@gmail.com>
2017-03-25 18:05:40 +0000

    Change sweep line changes to happen all at the same time to prevent errors on segmentation.

--- linklist.h
+++ linklist.h
@@ -5,6 +5,11 @@ typedef struct s_link_list{
 	half_edge *item;
 } link_list;
 
+typedef struct s_sweep_line_action{
+    int action;
+    half_edge *edge;
+} sweep_line_action;
+
 void addToList(link_list **head, half_edge *item);
 
 void addToListByX(link_list **head, half_edge *item);
@@ -12,3 +17,7 @@ void addToListByX(link_list **head, half_edge *item);
 void addToListByY(link_list **head, half_edge *item);
 
 void rmFromList(link_list **head, half_edge *item);
+
+int addEventSweepLine(sweep_line_action ** sweep_line_action_list, int size, int action, half_edge *edge);
+
+int runEventSweepLine(sweep_line_action ** sweep_line_action_list, int size, link_list ** sweep_line);
--- main.c
+++ main.c
@@ -76,6 +76,8 @@ void horizontalGrid(){
 	sortVertexListY(list, numberOfVertices);
 	printVertexList(list, numberOfVertices);
     link_list **sweep_line = malloc(sizeof(link_list*));
+    sweep_line_action ** sweep_line_action_list = malloc(sizeof(sweep_line_action*)*numberOfVertices);
+    int size_sweep_line_action = 0;
     (*sweep_line) = NULL;
 	while(i < numberOfVertices){
 		vertex** listToConsider;
@@ -98,35 +100,53 @@ void horizontalGrid(){
 		for(int j = 0; j < sizeToConsider; j++){
             link_list *tmp = *sweep_line;
             if(j > 0 && vertexConnected(listToConsider[j-1], listToConsider[j])){
-                vertex *a = listToConsider[j-1];
+                /* If next vertex are colinear, we must treat all as one edge */
+                int numberOfColinearVertex = 0;
+                while(j < sizeToConsider-1){
+                    if(vertexConnected(listToConsider[j], listToConsider[j+1])){
+                        j++;
+                        numberOfColinearVertex++;
+                    }
+                    else
+                        break;
+                }
+                vertex *a = listToConsider[j-1-numberOfColinearVertex];
                 vertex *b = listToConsider[j];
                 /* Case 9:
                  * When we can insert a new segment and there is no segmentation */
                 if(tmp == NULL){
-                    addToListByX(sweep_line, getUpEdge(a));
-                    addToListByX(sweep_line, getUpEdge(b));
+                    /* addToListByX(sweep_line, getUpEdge(a)); */
+                    /* addToListByX(sweep_line, getUpEdge(b)); */
+                    size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(a));
+                    size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(b));
                 }
                 while(tmp != NULL){
                     /* Case 5:
                      * When we can insert a new segment and there is no segmentation */
                     if(b->x < tmp->item->origin->x){
-                        addToListByX(sweep_line, getUpEdge(a));
-                        addToListByX(sweep_line, getUpEdge(b));
+                        /* addToListByX(sweep_line, getUpEdge(a)); */
+                        /* addToListByX(sweep_line, getUpEdge(b)); */
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(a));
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(b));
                         break;
                     }
                     /* Case 1:
                      * When the new segment is in between a segment in the sweep line. */
                     else if((a->x > tmp->item->origin->x) && (b->x < tmp->next->item->origin->x)){
                         /* NEED TO TRACE SEGMENTATION */
-                        addToListByX(sweep_line, getUpEdge(a));
-                        addToListByX(sweep_line, getUpEdge(b));
+                        /* addToListByX(sweep_line, getUpEdge(a)); */
+                        /* addToListByX(sweep_line, getUpEdge(b)); */
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(a));
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(b));
                         break;
                     }
                     /* Case 2:
                      * When the new segment is connected with its right side to sweep line. */
                     else if((a->x < tmp->item->origin->x) && (b->x == tmp->item->origin->x)){
-                        addToListByX(sweep_line, getUpEdge(a));
-                        rmFromList(sweep_line, tmp->item);
+                        /* addToListByX(sweep_line, getUpEdge(a)); */
+                        /* rmFromList(sweep_line, tmp->item); */
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(a));
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
                         break;
                     }
                     else if(tmp->next->next != NULL){
@@ -134,16 +154,20 @@ void horizontalGrid(){
                          * When the segment is connected with its left side to the sweep line. */
                         if((a->x == tmp->next->item->origin->x) && (b->x < tmp->next->next->item->origin->x)){
                             /* NEEDS SEGMENTATION */
-                            addToListByX(sweep_line, getUpEdge(b));
-                            rmFromList(sweep_line, tmp->next->item);
+                            /* addToListByX(sweep_line, getUpEdge(b)); */
+                            /* rmFromList(sweep_line, tmp->next->item); */
+                            size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(b));
+                            size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->next->item);
                             break;
                         }
                         /* Case 4:
                          * When the segment is connected both sides with the sweep line. */
                         else if((a->x == tmp->next->item->origin->x) && (b->x == tmp->next->next->item->origin->x)){
                             /* NEEDS SEGMENTATION */
-                            rmFromList(sweep_line, tmp->next->next->item);
-                            rmFromList(sweep_line, tmp->next->item);
+                            /* rmFromList(sweep_line, tmp->next->next->item); */
+                            /* rmFromList(sweep_line, tmp->next->item); */
+                            size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->next->next->item);
+                            size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->next->item);
                             break;
                         }
                     } 
@@ -151,30 +175,38 @@ void horizontalGrid(){
                      * When inside sweep line, connected at left */
                     if((a->x == tmp->item->origin->x) && (b->x < tmp->next->item->origin->x)){
                         /* NEEDS SEGMENTATION */
-                        rmFromList(sweep_line, tmp->item);
-                        addToListByX(sweep_line, getUpEdge(b));
+                        /* rmFromList(sweep_line, tmp->item); */
+                        /* addToListByX(sweep_line, getUpEdge(b)); */
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(b));
                         break;
                     }
                     /* Case 7:
                      * When inside sweep line, connected at right */
                     else if((a->x > tmp->item->origin->x) && (b->x == tmp->next->item->origin->x)){
                         /* NEEDS SEGMENTATION */
-                        addToListByX(sweep_line, getUpEdge(a));
-                        rmFromList(sweep_line, tmp->next->item);
+                        /* addToListByX(sweep_line, getUpEdge(a)); */
+                        /* rmFromList(sweep_line, tmp->next->item); */
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(a));
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->next->item);
                         break;
                     }
                     /* Case 8:
                      * when inside sweep line, connected at both sides, no segmentation */
                     else if((a->x == tmp->item->origin->x) && (b->x == tmp->next->item->origin->x)){
-                        rmFromList(sweep_line, tmp->next->item);
-                        rmFromList(sweep_line, tmp->item);
+                        /* rmFromList(sweep_line, tmp->next->item); */
+                        /* rmFromList(sweep_line, tmp->item); */
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->next->item);
+                        size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
                         break;
                     }
                     else{
                         tmp = tmp->next->next;
                         if(tmp == NULL){
-                            addToListByX(sweep_line, getUpEdge(a));
-                            addToListByX(sweep_line, getUpEdge(b));
+                            /* addToListByX(sweep_line, getUpEdge(a)); */
+                            /* addToListByX(sweep_line, getUpEdge(b)); */
+                            size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(a));
+                            size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(b));
                             break;
                         }
                     }
@@ -185,37 +217,46 @@ void horizontalGrid(){
                 while(tmp->item->origin->x != listToConsider[j]->x){
                     tmp = tmp->next;
                 }
-                rmFromList(sweep_line, tmp->item);
-                addToListByX(sweep_line, getUpEdge(listToConsider[j]));
+                /* rmFromList(sweep_line, tmp->item); */
+                /* addToListByX(sweep_line, getUpEdge(listToConsider[j])); */
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(listToConsider[j]));
                 /* NEEDS SEGMENTATION */
             }
             else if((j == 0) && !vertexConnected(listToConsider[j], listToConsider[j+1])){
                 while(tmp->item->origin->x != listToConsider[j]->x){
                     tmp = tmp->next;
                 }
-                rmFromList(sweep_line, tmp->item);
-                addToListByX(sweep_line, getUpEdge(listToConsider[j]));
+                /* rmFromList(sweep_line, tmp->item); */
+                /* addToListByX(sweep_line, getUpEdge(listToConsider[j])); */
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(listToConsider[j]));
                 /* NEEDS SEGMENTATION */
             }
             else if((j > 0) && (j < sizeToConsider-1) && !vertexConnected(listToConsider[j], listToConsider[j+1]) && !vertexConnected(listToConsider[j], listToConsider[j-1])){
                 while(tmp->item->origin->x != listToConsider[j]->x){
                     tmp = tmp->next;
                 }
-                rmFromList(sweep_line, tmp->item);
-                addToListByX(sweep_line, getUpEdge(listToConsider[j]));
+                /* rmFromList(sweep_line, tmp->item); */
+                /* addToListByX(sweep_line, getUpEdge(listToConsider[j])); */
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(listToConsider[j]));
                 /* NEEDS SEGMENTATION */
             }
             else if((j == sizeToConsider-1) && !vertexConnected(listToConsider[j], listToConsider[j-1])){
                 while(tmp->item->origin->x != listToConsider[j]->x){
                     tmp = tmp->next;
                 }
-                rmFromList(sweep_line, tmp->item);
-                addToListByX(sweep_line, getUpEdge(listToConsider[j]));
+                /* rmFromList(sweep_line, tmp->item); */
+                /* addToListByX(sweep_line, getUpEdge(listToConsider[j])); */
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 1, tmp->item);
+                size_sweep_line_action = addEventSweepLine(sweep_line_action_list, size_sweep_line_action, 0, getUpEdge(listToConsider[j]));
                 /* NEEDS SEGMENTATION */
             }
 		}
 
         printf("Printing sweep_line list:\n");
+        size_sweep_line_action = runEventSweepLine(sweep_line_action_list, size_sweep_line_action, sweep_line);
         link_list *tmp = *sweep_line;
         while(tmp != NULL){
             printf("Edge origin: (%d,%d) | destination: (%d, %d)\n", tmp->item->origin->x, tmp->item->origin->y, tmp->item->next->origin->x, tmp->item->next->origin->y);
@@ -225,3 +266,6 @@ void horizontalGrid(){
 		printVertexList(listToConsider, sizeToConsider);
 	}
 }
+
+
+
